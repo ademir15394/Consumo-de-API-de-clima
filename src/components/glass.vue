f64611b05c98f1402b1d91bae6f24e281fa3594e
ademir15394 <ademirzinhoperes@gmail.com>
2023-06-29 00:46:40 -0300

    commit

--- src/components/glass.vue
+++ src/components/glass.vue
@@ -3,10 +3,14 @@
 
     <div class="tittle">
 
-      <h1 class="title" v-if="texto">{{ texto.temp }}°C</h1>
+      <h1 class="title" v-if="tempe">{{ tempe.temp }} °C</h1>
+
+
 
     </div>
-
+    <div class="subsession">
+      <h1 class="subtitle" v-if="describe">{{ describe.description }}</h1>
+    </div>
 
   </div>
 </template>
@@ -20,7 +24,9 @@ export default {
 
     data(){
         return{
-            texto: null
+            tempe: null,
+            cidade: null,
+            describe: null
         }
     },
 
@@ -33,7 +39,9 @@ export default {
             const key = "135efbc7";
             const response =  await axios.get(`https://api.hgbrasil.com/weather?format=json-cors&key=${key}&lat=-&lon=-&user_ip=remote`)
 
-            this.texto = response.data.results;
+            this.tempe = response.data.results;
+            this.cidade = response.data.results;
+            this.describe = response.data.results;
 
         }catch(error) {
             console.log("Houve Erro na Aplicação" + error)
@@ -64,15 +72,34 @@ border-radius: 34px;
 .tittle{
   display:flex;
   justify-content: center;
-  margin-top: 30px;
+  margin-top: 20px;
 }
 
 .title{
+  color:#fff;
+  font-size: 70px;
+  display: flex;
+  text-align: center;
+  font-weight: lighter;
 
-  font-size: 60px;
+  background-image: linear-gradient(52deg, rgba(236,140,226,1) 19%, rgba(57,75,231,1) 94%);
+  /* agora colocamos o fundo gradiente dentro do texto com essa propriedade */
+  background-clip: text;
+  -webkit-background-clip: text;
+  /* a cor do texto deve estar como transparent */
+  color: transparent;
 
+}
 
-
+.subsession{
+  display: flex;
+  justify-content: center;
+  margin-top:22px;
+}
+.subtitle{
+  font-size: 30px;
+  color:#fff;
+  font-weight: normal;
 }
 
 </style>
